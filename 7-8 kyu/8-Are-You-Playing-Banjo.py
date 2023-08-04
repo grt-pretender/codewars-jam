f7029db1a428595132470e9e5f61e43885e5c868
Daria Mashkova <dmetafrasis@gmail.com>
2023-08-05 03:13:14 +0700

    Move file

--- 7-8 kyu/8-Are-You-Playing-Banjo.py
+++ 7-8 kyu/8-Are-You-Playing-Banjo.py
@@ -23,8 +23,9 @@ def are_you_playing_banjo(name):
 # 2) Another solution
 
 def are_you_playing_banjo(name):
-    if name[0].lower() == "r":
-        return name + " plays banjo"
-    else:
-        return name + " does not play banjo"
+	if name[0].lower() == "r":
+		return name + " plays banjo"
+	else:
+		return name + " does not play banjo"
+
 
