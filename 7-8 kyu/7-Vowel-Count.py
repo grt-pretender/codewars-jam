0f984f7bda4eb60b27467b383697a5ff247466eb
Daria Mashkova <dmetafrasis@gmail.com>
2023-08-05 18:21:55 +0700

    Add another solution

--- 7-8 kyu/7-Vowel-Count.py
+++ 7-8 kyu/7-Vowel-Count.py
@@ -2,6 +2,8 @@
 # We will consider a, e, i, o, u as vowels for this Kata (but not y).
 # The input string will only consist of lower case letters and/or spaces.
 
+# 1) solution
+
 def get_count(sentence):
 	
 	vowels = ["a", "e", "i", "o", "u"]
@@ -11,3 +13,11 @@ def get_count(sentence):
 			counter += 1
 
 	return counter
+
+# 2) using regex
+
+import re
+def get_count(sentence):
+	count = len(re.findall('[aeiou]', sentence))
+    return count
+
