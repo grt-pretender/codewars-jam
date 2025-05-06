db2f84bfce08557d069528e85d3b76fc2ecfbde8
Daria Mashkova <dmetafrasis@gmail.com>
2025-05-06 14:54:13 +0700

    Add file

---
+++ 7-8 kyu/8-Jenny`s-Secret-Message.js
@@ -0,0 +1,28 @@
+/* Jenny has written a function that returns a greeting for a user. 
+However, she's in love with Johnny, and would like to greet him slightly different. 
+She added a special case to her function, but she made a mistake.
+Can you help her?*/
+
+/* 
+==========================
+1) Simple if-else
+==========================
+*/
+
+function greet(name){
+  if(name === "Johnny")
+    return "Hello, my love!";
+  else 
+    return "Hello, " + name + "!";
+}
+
+
+/* 
+==========================
+2) Using ternary operator
+==========================
+*/
+
+function greet(name){
+  return "Hello, " + (name == "Johnny" ? "my love" : name) + "!";
+}
