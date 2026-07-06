61163ecc17f8bab9974aabfc70c9428520b7181a
Daria Mashkova <dmetafrasis@gmail.com>
2026-07-06 16:36:05 +0700

    Update 8-MakeUpperCase.go

--- 7-8 kyu/8-MakeUpperCase.go
+++ 7-8 kyu/8-MakeUpperCase.go
@@ -1,10 +1,9 @@
 // Write a function which converts the input string to uppercase.
 
-#include <string>
-#include <algorithm> 
+package kata
 
-std::string makeUpperCase(const std::string& str) {
-  std::string newString = str;
-  transform(newString.begin(), newString.end(), newString.begin(), ::toupper);
-  return newString;
+import "strings"
+
+func MakeUpperCase(str string) string {
+    return strings.ToUpper(str);
 }
